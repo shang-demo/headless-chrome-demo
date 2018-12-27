8cbf74d03c1a4f9ec001abba13b8bd93d3145b6e
xinshangshangxin <xinshangshangxin@gmail.com>
2018-12-27 10:12:46 +0800

    feat: init run puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-export {};
+import puppeteer from 'puppeteer';
+import { resolve as pathResolve } from 'path';
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
+  await page.goto('https://www.jd.com');
+  await page.setViewport({
+    width: 1200,
+    height: 800,
+  });
+
+  await autoScroll(page);
+
+  await page.screenshot({
+    path: pathResolve(__dirname, '../jd.png'),
+    fullPage: true,
+  });
+
+  await browser.close();
+})();
+
+async function autoScroll(page: puppeteer.Page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve) => {
+      let totalHeight = 0;
+      let distance = 200;
+      let timer = setInterval(() => {
+        let { scrollHeight } = document.body;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}
